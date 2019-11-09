86c093667c7217190b981b00e7fab41692ba8b48
João Victor <jvsouzax@gmail.com>
2019-11-09 11:14:30 -0300

    add pt br

--- crypo_wallet/app/controllers/welcome_controller.rb
+++ crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,6 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    @nome = "João"
-    # @nome = params[:nome]
   end
 end
--- crypo_wallet/app/helpers/application_helper.rb
+++ crypo_wallet/app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-    def date_br(data_us)
-        data_us.strftime("%d/%m/%Y")
-    end
 end
