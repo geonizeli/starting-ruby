054058f2fd0317360731de601f81024c8bc077a2
João Victor <jvsouzax@gmail.com>
2019-11-16 10:32:21 +0000

    pre-compilando e isolando assets por controller

--- crypo_wallet/config/initializers/assets.rb
+++ crypo_wallet/config/initializers/assets.rb
@@ -12,3 +12,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( welcome.js
+                                                  coins.js
+                                                  mining_types.js
+                                                  scaffold.js )
+
+Rails.application.config.assets.precompile += %w( welcome.css
+                                                  coins.css
+                                                  mining_types.css
+                                                  scaffold.css )
